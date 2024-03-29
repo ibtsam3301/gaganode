e6e6b9b401f5526200376ff41a5349fc1d733e49
Ibtsam Ahmad <ahmadibtsam07@gmail.com>
2024-03-29 21:19:56 +0500

    Update gaganode_install.sh

--- gaganode_install.sh
+++ gaganode_install.sh
@@ -1,50 +1,22 @@
 #!/bin/bash
 
-cpu_arch="$(uname -m)"
-echo "cpu_arch=$cpu_arch"
-AMD="https://assets.coreservice.io/public/package/66/gaganode_pro/0.0.300/gaganode_pro-0_0_300.tar.gz"
-ARM="https://assets.coreservice.io/public/package/68/gaganode_pro/0.0.300/gaganode_pro-0_0_300.tar.gz"
+# Exit script on any error
+set -e
 
-if [ "${cpu_arch}" = "x86_64" ]; then
-DOWNLOADLINK=$AMD
-FILENAME="app-linux-amd64.tar.gz"
-elif [[ "${cpu_arch}" = "arm64" ]] || [[ "${cpu_arch}" = "aarch64" ]]; then
-DOWNLOADLINK=$ARM
-FILENAME="app-linux-arm64.tar.gz"
-else
-echo "Your CPU type dose not support" 
-exit 1
-fi
+# Step 1: Download the package
+wget https://assets.coreservice.io/public/package/68/gaganode_pro/0.0.300/gaganode_pro-0_0_300.tar.gz
 
-echo "DOWNLOADLINK=$DOWNLOADLINK"
+# Step 2: Extract the package
+tar xzvf gaganode_pro-0_0_300.tar.gz
 
-if [ -d ./apphub-linux* ]; then
-echo "apphub-linux found" &&
-cd ./apphub-linux* &&
-./apphub service start &&
-./apphub status &&
-./apphub log &&
-./apps/gaganode/gaganode log &&
-cat ./apps/gaganode/user_conf/default.toml
-else
-echo "apphub-linux NOT found."
-curl -o $FILENAME $DOWNLOADLINK &&
-tar -zxf $FILENAME &&
-rm -f $FILENAME &&
-cd ./apphub-linux* &&
-./apphub service remove &&
-./apphub service install
-./apphub service start
-./apphub status &&
-sleep 30 && echo ' ____   ___  _   _ _____
-|  _ \ / _ \| \ | | ____|
-| | | | | | |  \| |  _|
-| |_| | |_| | |\  | |___
-|____/ \___/|_| \_|_____|'
-./apphub status && 
-./apps/gaganode/gaganode config set --token=$TOKEN &&
-./apphub restart &&
-./apps/gaganode/gaganode log
-fi
+# Step 3: Change directory
+cd gaganode-linux-amd64
 
-/bin/bash
+# Step 4: Update the configuration file with the token from environment variable
+# Replace 'your_token_variable' with the actual name of your environment variable
+sed -i "s/token='[^']*'/token='$TOKEN'/g" root_conf/default.toml
+
+# Step 5: Run the gaganode process
+# Here, I'm directly running the command without nohup. If you want this script to exit and leave gaganode running,
+# you might consider different strategies depending on your Docker setup.
+./gaganode &
